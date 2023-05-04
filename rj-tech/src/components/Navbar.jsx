0563c00915499254cb90eb94a06d4333b7a3aea8
Radosav Perisic <radosavperisic00@gmail.com>
2023-05-04 16:12:51 +0200

    Add styles to Navbar

--- rj-tech/src/components/Navbar.jsx
+++ rj-tech/src/components/Navbar.jsx
@@ -4,8 +4,8 @@ import {HiOutlineShoppingCart} from 'react-icons/hi'
 
 const Navbar = () => {
   return (
-    <div className=''>
-        <div className=''>
+    <div className='w-full h-20 bg-[#2b2a2a] flex justify-end items-center'>
+        <div className='mr-12 flex items-center'>
         <Link to='/'>Shop</Link>
         <Link to='/cart'><HiOutlineShoppingCart size={32}/></Link>
         </div>
