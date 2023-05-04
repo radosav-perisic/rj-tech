31c436d657905f9f0ef5eacd95fed93d39160167
Radosav Perisic <radosavperisic00@gmail.com>
2023-05-04 22:12:57 +0200

    Create Products component & add images

---
+++ rj-tech/src/products.js
@@ -0,0 +1,59 @@
+import peach12 from "../src/assets/peach12.jpg";
+import red13 from "../src/assets/red13.jpg";
+import capriblue12 from "../src/assets/capriblue12.jpg";
+import deepnavy12 from "../src/assets/deepnavy12.jpg";
+import purple12 from "../src/assets/12purple.jpg";
+import black12 from "../src/assets/black12.jpg";
+import pink12 from "../src/assets/pink12.jpg";
+import orange12 from "../src/assets/orange12.jpg";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Iphone 12 Peach Case",
+    price: 39.99,
+    productImage: peach12,
+  },
+  {
+    id: 2,
+    productName: "Iphone 13 Red Case",
+    price: 39.99,
+    productImage: red13,
+  },
+  {
+    id: 3,
+    productName: "Iphone 12 Capri Blue Case",
+    price: 39.99,
+    productImage: capriblue12,
+  },
+  {
+    id: 4,
+    productName: "Iphone 12 Deep Navy Case",
+    price: 39.99,
+    productImage: deepnavy12,
+  },
+  {
+    id: 5,
+    productName: "Iphone 12 Purple Case",
+    price: 39.99,
+    productImage: purple12,
+  },
+  {
+    id: 6,
+    productName: "Iphone 12 Black Case",
+    price: 39.99,
+    productImage: black12,
+  },
+  {
+    id: 7,
+    productName: "Iphone 12 Pink Case",
+    price: 39.99,
+    productImage: pink12,
+  },
+  {
+    id: 8,
+    productName: "Iphone 12 Orange Case",
+    price: 39.99,
+    productImage: orange12,
+  },
+];
