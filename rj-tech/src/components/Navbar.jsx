e4d88ec8fd5d5bbf1cb6c1492378bec3a751136e
Radosav Perisic <radosavperisic00@gmail.com>
2023-05-04 16:08:27 +0200

    Add links & react icon to Navbar

--- rj-tech/src/components/Navbar.jsx
+++ rj-tech/src/components/Navbar.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import {HiOutlineShoppingCart} from 'react-icons/hi'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className=''>
+        <div className=''>
+        <Link to='/'>Shop</Link>
+        <Link to='/cart'><HiOutlineShoppingCart size={32}/></Link>
+        </div>
+    </div>
   )
 }
 
