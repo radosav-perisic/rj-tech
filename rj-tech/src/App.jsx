bf629588ce4c0e236792859e83e80b2774abc9df
Radosav Perisic <radosavperisic00@gmail.com>
2023-05-04 16:04:43 +0200

    Add routes to app

--- rj-tech/src/App.jsx
+++ rj-tech/src/App.jsx
@@ -1,11 +1,16 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 function App() {
  
 
   return (
       <div className="">
-       
+       <Router>
+        <Routes>
+          <Route path='/'/>
+        <Route path='/cart'/>
+        </Routes>
+       </Router>
       </div>
   )
 }
