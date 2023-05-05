ab6a73f3db3be9546478091482daa430eabf1848
Radosav Perisic <radosavperisic00@gmail.com>
2023-05-05 16:19:53 +0200

    Add addToCart and removeFromCart buttons
    Add addToCart and removeFromCart buttons to CartItem and implement the logic for the buttons using useContext

--- rj-tech/src/components/CartItem.jsx
+++ rj-tech/src/components/CartItem.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, {useContext} from "react";
+import { Context } from "../context/Context";
 
 const CartItem = (props) => {
+    const {cartItems, addToCart, removeFromCart} =useContext(Context)
+
   const { id, productName, price, productImage } = props.data;
   return (
     <div className="w-[700px] h-[250px] flex items-center shadow-2xl rounded-3xl m-[30px]">
@@ -10,6 +13,11 @@ const CartItem = (props) => {
             {productName}
         </p>
         <p>${price}</p>
+        <div className="countHandler">
+           <button onClick={()=> removeFromCart(id)}> - </button>
+           <input value={cartItems[id]}/>
+           <button onClick={()=> addToCart(id)}>  + </button>
+        </div>
       </div>
     </div>
   );
