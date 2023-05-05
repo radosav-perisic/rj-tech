2e07dd764e906dc79e5b3aee6b32b9b886a0e14a
Radosav Perisic <radosavperisic00@gmail.com>
2023-05-05 10:54:23 +0200

    Map Products array inside of shop component

--- rj-tech/src/pages/Shop.jsx
+++ rj-tech/src/pages/Shop.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { PRODUCTS } from "../products";
+import Product  from "../components/product";
 
 const Shop = () => {
   return (
-    <div>Shop</div>
-  )
-}
+    <div className="">
+      <div className="">
+        <h1>R&J Tech Store</h1>
+      </div>
+      <div className="">
+        {PRODUCTS.map((product) => (
+          <Product data={product}/>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Shop
+export default Shop;
