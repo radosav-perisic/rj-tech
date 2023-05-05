2e73c968819327676a8c7f9f73e927bbe9f57c6b
Radosav Perisic <radosavperisic00@gmail.com>
2023-05-05 16:14:07 +0200

    Update CartItem to display product information

--- rj-tech/src/components/CartItem.jsx
+++ rj-tech/src/components/CartItem.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
-const CartItem = () => {
+const CartItem = (props) => {
+  const { id, productName, price, productImage } = props.data;
   return (
-    <div>cart-item</div>
-  )
-}
+    <div className="w-[700px] h-[250px] flex items-center shadow-2xl rounded-3xl m-[30px]">
+      <img className="w-[200px]" src={productImage} />
+      <div className="w-full text-3xl">
+        <p>
+            {productName}
+        </p>
+        <p>${price}</p>
+      </div>
+    </div>
+  );
+};
 
-export default CartItem
+export default CartItem;
