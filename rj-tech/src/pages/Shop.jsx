be770640926b0cc7654af7dcbad5570e78c3bddc
Radosav Perisic <radosavperisic00@gmail.com>
2023-05-05 13:53:52 +0200

    Add responsive design to Shop component

--- rj-tech/src/pages/Shop.jsx
+++ rj-tech/src/pages/Shop.jsx
@@ -4,11 +4,11 @@ import Product  from "../components/product";
 
 const Shop = () => {
   return (
-    <div className="">
-      <div className="">
-        <h1>R&J Tech Store</h1>
+    <div className="w-full h-screen justify-center items-center">
+      <div className="mt-24 text-center text-5xl">
+        <h1>R&J Tech </h1>
       </div>
-      <div className="">
+      <div className="w-full h-auto grid grid-cols-2 gap-x-3 mt-12 sm:grid-cols-3 gap-12 text-center font-semibold py-6 items-center mx-auto justify-center">
         {PRODUCTS.map((product) => (
           <Product data={product}/>
         ))}
