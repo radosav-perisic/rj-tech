190d73ce9202af11dec485961f99ae75865173ad
Radosav Perisic <radosavperisic00@gmail.com>
2023-05-05 13:56:28 +0200

    Improved responsiveness & styling of Product

--- rj-tech/src/components/Product.jsx
+++ rj-tech/src/components/Product.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
 const Product = (props) => {
-  const { id, product, price, productImage } = props.data;
+  const { id, productName, price, productImage } = props.data;
   return (
-    <div className="">
-      <img src={productImage} />
-      <div className="">
+    <div className="rounded-2xl lg:w-[300px] lg:h-[350px] lg:m-[100px] w-[120px] h-[170px] mx-auto flex-col justify-center flex items-center transition ease-in duration-300">
+      <img className="w-full max-w-[400px]" src={productImage} />
+      <div className="text-center">
         <p>{productName}</p>
-        <p>{price}</p>
+        <p>${price}</p>
       </div>
     </div>
   );
