88dc4415a5b294c9b006fc68989f9a856a57714d
Radosav Perisic <radosavperisic00@gmail.com>
2023-05-07 17:32:26 +0200

    Update Shop component responsiveness and style

--- rj-tech/src/pages/Shop.jsx
+++ rj-tech/src/pages/Shop.jsx
@@ -5,10 +5,10 @@ import Product  from "../components/product";
 const Shop = () => {
   return (
     <div className="w-full h-screen justify-center items-center">
-      <div className="mt-24 text-center text-5xl">
+      <div className="pt-32 text-center text-5xl">
         <h1>R&J Tech </h1>
       </div>
-      <div className="w-full h-auto grid grid-cols-2 gap-x-3 mt-12 sm:grid-cols-3 gap-24 text-center font-semibold py-6 items-center mx-auto justify-center">
+      <div className="w-full max-w-[1000px] h-auto grid grid-cols-2 gap-x-3 mt-12 sm:grid-cols-3 gap-24 text-center font-semibold py-6 items-center mx-auto justify-center">
         {PRODUCTS.map((product) => (
           <Product data={product}/>
         ))}
