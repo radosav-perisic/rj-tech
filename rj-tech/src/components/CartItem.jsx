eb328ddd9687560da6431a5b06c2cb14ef0de798
Radosav Perisic <radosavperisic00@gmail.com>
2023-10-24 19:05:27 +0200

    Update CartItem

--- rj-tech/src/components/CartItem.jsx
+++ rj-tech/src/components/CartItem.jsx
@@ -6,16 +6,16 @@ const CartItem = (props) => {
 
   const { id, productName, price, productImage } = props.data;
   return (
-    <div className="w-[700px] h-[250px] flex items-center shadow-2xl rounded-3xl m-[30px]">
+    <div className="lg:w-[700px] lg:h-[250px] w-[220px] h-[60px] flex items-center shadow-lg rounded-3xl mt-[70px]">
       <img className="w-[200px]" src={productImage} />
       <div className="w-full text-3xl">
         <p>
             {productName}
         </p>
         <p>${price}</p>
-        <div className="countHandler">
+        <div  className="flex">
            <button onClick={()=> removeFromCart(id)}> - </button>
-           <input value={cartItems[id]} onChange={(e) => updateCartItem(Number(e.target.value), id)}/>
+           <input  className="w-10 text-center mt-16 outline-slate-600 outline-1" value={cartItems[id]} onChange={(e) => updateCartItem(Number(e.target.value), id)}/>
            <button onClick={()=> addToCart(id)}>  + </button>
         </div>
       </div>
