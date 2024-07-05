79e7909d006cc0e9ace368a600ecd7e489568dfa
Radosav Perisic <radosavperisic00@gmail.com>
2024-07-05 20:51:51 +0200

    Update Navbar

--- rj-tech/src/components/Navbar.jsx
+++ rj-tech/src/components/Navbar.jsx
@@ -1,33 +1,31 @@
-import React, {useContext} from 'react'
-import { Link } from 'react-router-dom'
-import {HiOutlineShoppingCart} from 'react-icons/hi'
-import { Context } from '../context/Context'
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { HiOutlineShoppingCart } from 'react-icons/hi';
+import { Context } from '../context/Context';
 
 const Navbar = () => {
-  const {numItems } = useContext(Context);
+  const { numItems } = useContext(Context);
 
   return (
-    <div className='w-full h-[80px] z-10 px-4 bg-[#96abe8] fixed flex justify-between items-center'>
-            <div className='flex-wrap items-center font-semibold ml-6'>
+    <div className="w-full h-[80px] z-10 px-4 bg-[#96abe8] fixed flex justify-between items-center">
+      <div className="flex-1 flex justify-center items-center gap-3 font-semibold">
         <Link to='/'>Shop</Link>
-        
-        </div>
-        <div className='mr-12 flex items-center'>
-    
-        <Link to='/cart'><HiOutlineShoppingCart className='text-white  duration-300' size={42}/><div
-              className="rounded-full bg-[#e28119] border-[1px] border-[#ffffff] flex justify-center items-center absolute h-[1.10rem] w-[1.10rem] text-sm font-medium text-white"
-              style={{
-               
-                transform: "translate(155%, -65%)",
-              }}
-            >
-              {numItems}
-            </div>
-          </Link>
-        
-        </div>
+        <Link to='/'>Shop</Link>
+        <Link to='/'>Shop</Link>
+      </div>
+      <div className="flex items-center mr-6 relative">
+        <Link to='/cart'>
+          <HiOutlineShoppingCart className='text-white duration-300' size={42} />
+          <div
+            className="rounded-full bg-[#e28119] border-[1px] border-[#ffffff] flex justify-center items-center absolute h-[1.10rem] w-[1.10rem] text-sm font-medium text-white"
+            style={{ transform: "translate(155%, -65%)" }}
+          >
+            {numItems}
+          </div>
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
