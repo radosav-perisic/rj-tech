a62648dac56cd68cbc69c75e8588153d11f473c8
Radosav Perisic <radosavperisic00@gmail.com>
2024-07-08 16:39:02 +0200

    add link to home page & about us in navbar

--- rj-tech/src/components/Navbar.jsx
+++ rj-tech/src/components/Navbar.jsx
@@ -8,13 +8,15 @@ const Navbar = () => {
 
   return (
     <div className="w-full h-[80px] z-10 px-4 bg-[#96abe8] fixed flex justify-between items-center">
-      <div className="flex-1 text-lg font flex justify-center items-center gap-7 font-semibold">
+      <div className="flex-1 text-base md:text-lg font flex justify-center items-center gap-2 md:gap-7 font-semibold">
+        <Link className="hover-effect mr-14" to='/'>Home</Link>
         <Link className="hover-effect" to='/airpods'>Airpods</Link>
         <Link className="hover-effect" to='/cases'>Cases</Link>
         <Link className="hover-effect" to='/chargers'>Chargers</Link>
-        <Link className="hover-effect" to='/gadgets'>Gadgets</Link>
+        <Link className="hover-effect" to='/accessories'>Accessories</Link>
       </div>
-      <div className="flex items-center mr-6 relative">
+      <div className="flex items-center mr-6 relative gap-7 font-semibold">
+      <Link className="hover-effect" to='/about'>About us</Link>
         <Link to='/cart'>
           <HiOutlineShoppingCart className='text-white duration-300' size={42} />
           <div
