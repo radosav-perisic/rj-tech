6def1e5bd154ccfc612f51bdd33842ff78d59c1a
Radosav Perisic <radosavperisic00@gmail.com>
2024-07-19 22:58:04 +0200

    add new routes to app.jsx

--- rj-tech/src/App.jsx
+++ rj-tech/src/App.jsx
@@ -1,13 +1,16 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Cart from "./pages/Cart";
 import Shop from "./pages/Shop";
 import Checkout from "./pages/Checkout";
+import LoyaltyProgram from "./pages/LoyaltyProgram";
 import ContextProvider from './context/Context';
 import Airpods from "./pages/Airpods";
 import Accessories from "./pages/Accessories";
 import Cases from "./pages/Cases";
 import Chargers from "./pages/Chargers";
+import Watches from './pages/Watches';
 
 function App() {
   return (
@@ -21,8 +24,10 @@ function App() {
             <Route path="/cases" element={<Cases />} />
             <Route path="/chargers" element={<Chargers />} />
             <Route path="/accessories" element={<Accessories />} />
+            <Route path="/watches" element={<Watches />} />
             <Route path="/cart" element={<Cart />} />
-            <Route path="/checkout" element={<Checkout />} />
+            <Route path='/checkout' element={<Checkout />} />
+            <Route path='/loyalty-program' element={<LoyaltyProgram />} />
           </Routes>
         </Router>
       </ContextProvider>
