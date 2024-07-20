95a3c4bd99fab7eb2f57ef94728499e3bb972354
Radosav Perisic <radosavperisic00@gmail.com>
2024-07-20 14:08:02 +0200

    Update import for Products & add title to Chargers category

--- rj-tech/src/pages/Chargers.jsx
+++ rj-tech/src/pages/Chargers.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { PRODUCTS } from "../products";
-import Product from "../products";
+import PRODUCTS from "../products";
+import Product from "../components/Product";
+
 
 const Chargers = () => {
   const chargersProducts = PRODUCTS.filter(product => product.category === "Chargers");
   return (
     <div className="w-full h-full rounded flex flex-col justify-center items-center">
+      <h1 className="text-6xl font-bold mb-10">CHARGERS</h1>
       <div className="w-full max-w-[1000px] mt-20 grid grid-cols-2 gap-x-3 h-auto sm:grid-cols-3 gap-24 text-center font-semibold py-6 items-center mx-auto justify-center">
         {chargersProducts.map(product => (
           <Product key={product.id} data={product} />
