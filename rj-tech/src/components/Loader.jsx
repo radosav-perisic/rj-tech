0d216dd20ccae083f4fa6d87242672b152f41470
Radosav Perisic <radosavperisic00@gmail.com>
2024-08-17 12:28:38 +0200

    Create Loader.jsx

---
+++ rj-tech/src/components/Loader.jsx
@@ -0,0 +1,13 @@
+// src/components/Loader.jsx
+import React from 'react';
+import '../index.css'; // Ensure this file imports the CSS containing the loader styles
+
+const Loader = () => {
+  return (
+    <div className="loader-container">
+      <div className="loader"></div>
+    </div>
+  );
+};
+
+export default Loader;
